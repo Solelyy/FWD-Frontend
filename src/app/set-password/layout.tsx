ceeb55a18fec600ebc991d083be59f08e8bcc841
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2026-03-08 19:37:24 +0800

    add: toaster for feedback display

--- src/app/set-password/layout.tsx
+++ src/app/set-password/layout.tsx
@@ -1,6 +1,10 @@
+import { Toaster } from "sonner"
 export const metadata = {
     title: "Set Password",
 }
 export default function SetPasswordLayout({children}: {children:React.ReactNode}) {
-   return <>{children}</>
+   return <>
+   {children}
+   <Toaster richColors position= "top-center" />
+   </>
 }
