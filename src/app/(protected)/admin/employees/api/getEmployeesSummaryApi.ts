dbdb140a58c739129f21b5cbb7fb07d553643bb2
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2026-04-24 19:41:47 +0800

    ft: card summary

---
+++ src/app/(protected)/admin/employees/api/getEmployeesSummaryApi.ts
@@ -0,0 +1,17 @@
+import { EmployeesSummaryResponse } from "../types/employees";
+import { API_BASE_URL } from "@/lib/util/api";
+
+export async function getEmployeesSummaryApi(): Promise<EmployeesSummaryResponse>{
+    const endpoint =  `/`;
+    const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+        method: "GET",
+        credentials: "include"
+    });
+        
+    if (!response.ok) throw new Error ("Cannot fetch employees accounts summary.");
+        
+    const result = await response.json();
+    console.log("Fetch employees accs summary: ", result);
+        
+    return result;   
+}
